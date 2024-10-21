30ef52f433ce7bdde9d2a6e4a12f5c600ee2f7d3
Bhagyashri <33651055+Btushir@users.noreply.github.com>
2024-10-21 09:23:39 -0700

    summertic-tree-bfs

--- symmetric_tree.py
+++ symmetric_tree.py
@@ -1,12 +1,14 @@
 """
-BFS: at each level, it should be a palindrome. TO show that, if a node has no child, then add them as NULL.
+BFS: At each level of tree, it should be a palindrome. To show that, if a node has no child, then add them as NULL.
 The whole node is added to the list to be checked as palindrome.
+
+Optimized BFS:
+
+Todo: DFS
 """
 
 from collections import deque
 from typing import Optional
-
-
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -15,6 +17,34 @@ from typing import Optional
 #         self.left = left
 #         self.right = right
 class Solution:
+    def bfs(self, root):
+        q = deque()
+        # add both left and right
+        q.append(root.left)
+        q.append(root.right)
+
+        while q:
+            # pop 2 numbers from the queue to compare
+            left = q.popleft()
+            right = q.popleft()
+
+            # one of them could be null
+            if (not left and right) or (not right and left):
+                return False
+            # both of them are null
+            elif not left and not right:
+                continue
+            # the value is not equal
+            elif left.val != right.val:
+                return False
+
+            q.append(left.left)
+            q.append(right.right)
+            q.append(left.right)
+            q.append(right.left)
+
+        return True
+
 
     def bfs(self, root):
         q = deque()
